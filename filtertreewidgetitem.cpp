ee63fc4a813c16a15d4a6683e696a5a4af9e07e3
BrainBone <brainb0ne@gmail.com>
2012-01-30 23:41:30 +0100

    added GPL License text (comment)

--- filtertreewidgetitem.cpp
+++ filtertreewidgetitem.cpp
@@ -1,3 +1,19 @@
+/*
+ * Spotweb Filter Creator
+ * Copyleft 2012
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * For more information about the GNU General Public License: <http://www.gnu.org/licenses/>.
+ */
+
 #include "filtertreewidgetitem.h"
 
 FilterTreeWidgetItem::FilterTreeWidgetItem(QTreeWidget * parent, int type)
